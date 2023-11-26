d161011a8ba17a30610baa374b31865bf9b88ca7
wbalconi <wbalconi@3arrows.us>
2023-11-26 10:28:54 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Write a program and ask the user to enter a few words separated by a space. Use the words to create a variable name with PascalCase. For example, if the user types: "number of students", display "NumberOfStudents". Make sure that the program is not dependent on the input. So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+*/
+
+using System;
+using System.Collections.Generic;
+
+Console.Write("Enter a few words separated by a space: ");
+var input = Console.ReadLine().Split(" ");
+var PascalCase = "";
+for (var i = 0; i < input.Length; i++)
+{
+    var word = input[i].ToLower();
+    var wordPascalCase = char.ToUpper(word[0]) + word.Substring(1);
+    PascalCase += wordPascalCase;
+}
+Console.WriteLine(PascalCase);
